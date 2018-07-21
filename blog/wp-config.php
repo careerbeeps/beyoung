4e388ca8af64956c93509977332b3e40a5a65411
Yaseen Ahmad <yaseen.ahmad2009@gmail.com>
2018-07-22 00:46:09 +0530

    Security updates

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -36,7 +36,7 @@ define('DB_NAME', 'beyoung_blog');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'RT2x5w7!');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
